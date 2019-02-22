626f6145e32e3bf8550dce254a2f2f79de61a717
Lucio Ticali <info@freewebsolution.it>
2019-02-22 09:21:47 +0100

    Aggiunto cartella admin 2 tentativo

---
+++ src/app/Admin/phone/phone.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { Device } from '../model/device';
+import { PhoneService } from '../service/phone.service';
+
+
+@Component({
+  selector: 'app-phone',
+  templateUrl: './phone.component.html',
+  styleUrls: ['./phone.component.scss']
+})
+export class PhoneComponent implements OnInit {
+  devices: Device[];
+  active: Device;
+  showloader: boolean;
+
+  showerrmsg: string;
+
+  constructor(private deviceservice: PhoneService) {
+    this.showloader = true;
+  }
+
+  ngOnInit() {
+    this.getAll();
+  }
+
+  getAll() {
+    this.deviceservice.getAll()
+      .subscribe(dati => {
+        this.devices = dati;
+        this.showloader = false;
+      }, error => this.showerrmsg = error
+      );
+  }
+  delete(event: MouseEvent, device: Device) {
+    event.stopPropagation();
+    this.deviceservice.deleteDevice(device)
+      .subscribe(
+        () => {
+          const id = this.devices.indexOf(device);
+          this.devices.splice(id, 1);
+        }
+      );
+      }
+
+  okFormPadre(obj: Object) {
+    if (obj['action'] === 'add' || obj['action'] === 'update') {
+      this.getAll();
+    }
+
+  }
+  setActive(device: Device) {
+    this.active = device;
+  }
+
+
+}
